4eaac34f1a7d59b792fe137f87bda515ae621438
jakegornall <jakegornall@yahoo.com>
2016-11-10 18:47:48 -0500

    Add top nav bar
    not yet functional

--- static/js/LonesMainPageController.js
+++ static/js/LonesMainPageController.js
@@ -2,41 +2,56 @@
 var isVisable = false;
 
 $(document).ready(function() {
-	$('#menu-icon').click(function() {
-		if (isVisable === false){
-			$('#nav-bar').animate({
-				marginLeft: "0px",
-				height: "45px"
-			});
-			setTimeout(function(){
-				$('#nav-bar').css('height', 'auto');
-			}, 501)
-			
+    $('#menu-icon').click(function() {
+        if (isVisable === false){
+            $('#nav-bar').animate({
+                marginLeft: "0px",
+                height: "45px"
+            });
+            setTimeout(function(){
+                $('#nav-bar').css('height', 'auto');
+            }, 501)
+            
 
-			isVisable = true;
-		} else {
-			$('#nav-bar').animate({
-				marginLeft: "-150%",
-				height: "0px"
-			});
-			isVisable = false;
-		}
-	})
+            isVisable = true;
+        } else {
+            $('#nav-bar').animate({
+                marginLeft: "-150%",
+                height: "0px"
+            });
+            isVisable = false;
+        }
+    })
 })
 
 $(window).resize(function() {
-	if ($(window).width() > 992) {
-		$('#nav-bar').css('height', 'auto');
-		$('#nav-bar').css('margin-left', '0px');
-		$('.menu-item').css('height', 'auto');
-	} else if (isVisable === true) {
-		$('#nav-bar').css('height', 'auto');
-		$('#nav-bar').css('margin-left', '0px');
-	} else {
-		$('#nav-bar').css('height', '0px');
-		$('#nav-bar').css('margin-left', '-150%');
-	}
+    if ($(window).width() > 992) {
+        $('#nav-bar').css('height', 'auto');
+        $('#nav-bar').css('margin-left', '0px');
+        $('.menu-item').css('height', 'auto');
+    } else if (isVisable === true) {
+        $('#nav-bar').css('height', 'auto');
+        $('#nav-bar').css('margin-left', '0px');
+    } else {
+        $('#nav-bar').css('height', '0px');
+        $('#nav-bar').css('margin-left', '-150%');
+    }
 })
+var fixedNavBar = $('#fixed-nav-bar');
+// shows top nav bar when scrolled past 400px.
+
+$(window).scroll(function () { 
+    if ($(window).scrollTop() > 300) {
+        fixedNavBar.animate({
+            top: "0px"
+        });
+    } else {
+        fixedNavBar.animate({
+            top: "-60px"
+        });
+    }
+});
+
 
 
 // creates smooth window scroll when a tag is clicked.
